62908ddd83932bf74ee2127dcc473814786c73af
Eliezer Fuentes <eliezermario008@gmail.com>
2022-09-28 10:09:04 -0400

    commit

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,12 +2,7 @@ import React from "react";
 import "./TodoList.css";
 
 function TodoList(props) {
-    return (
-        <section>
-
-            {props.children}
-        </section>
-    )
+	return <section>{props.children}</section>;
 }
 
 export { TodoList };
--- src/components/TodoSearch/TodoSearch.js
+++ src/components/TodoSearch/TodoSearch.js
@@ -4,29 +4,21 @@ import { TodoContext } from "../TodoContext/TodoContext";
 import "./TodoSearch.css";
 
 function TodoSearch() {
-    const { searchValue, setSearchValue } = useContext(TodoContext);
-    const onChangeSearchTodo = (event) => {
-        setSearchValue(event.target.value);
-    }
-    return (
-        <React.Fragment>
-            <div>
-                <div className="div-checkbox">
-                    <label >Completados</label>
-                    <br />
-                    <label class="toggler-wrapper style-1">
-                        <input type="checkbox" />
-                        <div class="toggler-slider">
-                            <div class="toggler-knob"></div>
-                        </div>
-                    </label>
-
-                </div>
-            </div>
-
-            <input type="search" onChange={onChangeSearchTodo} value={searchValue} placeholder="Buscar Tarea" className="input-search-todo" />
-        </React.Fragment>
-    )
+	const { searchValue, setSearchValue } = useContext(TodoContext);
+	const onChangeSearchTodo = (event) => {
+		setSearchValue(event.target.value);
+	};
+	return (
+		<React.Fragment>
+			<input
+				type='search'
+				onChange={onChangeSearchTodo}
+				value={searchValue}
+				placeholder='Buscar Tarea'
+				className='input-search-todo'
+			/>
+		</React.Fragment>
+	);
 }
 
 export { TodoSearch };
